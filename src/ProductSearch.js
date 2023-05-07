2e0c081e1f8c550fcd84f2b9a4204d9fab5527f3
Abhishek Paswan <abhishekgahlaut248@gmail.com>
2023-05-07 09:21:29 +0530

    product

---
+++ src/ProductSearch.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const ProductSearch = () => {
+  const [products, setProducts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get('https://fakestoreapi.com/products');
+      setProducts(response.data);
+    } catch (error) {
+      console.error('error');
+    }
+  };
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>Product Search</h1>
+      <input
+        type="text"
+        placeholder="Search products..."
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      <ul>
+        {filteredProducts.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductSearch;
